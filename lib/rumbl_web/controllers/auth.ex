dcd94720745a82df424cc597a3bbc117a692bbc4
dennistarsang <sangdennis9@gmail.com>
2022-03-29 21:51:01 +0300

    ADDED: Added  function

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -17,4 +17,15 @@ defmodule RumblWeb.Auth do
     user = user_id && Rumbl.Accounts.get_user(user_id)
     assign(conn, :current_user, user)
   end
+
+  @doc """
+  Receives the connection and the user, and stores the user ID
+  in the system.
+  """
+  def login(conn, user) do
+    conn
+    |> assign(:current_user, user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 end
