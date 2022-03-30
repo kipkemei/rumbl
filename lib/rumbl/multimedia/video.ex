397932e368558388a43e026e911f9f4003fc0b1e
dennistarsang <sangdennis9@gmail.com>
2022-03-30 15:00:47 +0300

    ADDED: Added function documentation to .

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -6,12 +6,25 @@ defmodule Rumbl.Multimedia.Video do
     field :description, :string
     field :title, :string
     field :url, :string
-    field :user_id, :id
+    belongs_to :user, Rumbl.Accounts.User
 
     timestamps()
   end
 
-  @doc false
+  @spec changeset(
+          {map, map}
+          | %{
+              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
+              optional(atom) => any
+            },
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
+        
+  @doc """
+  The cast function prepares tainted user input containing your specified fields for safe inclusion into the db.
+  cast uses a whitelist to tell Ecto which fields from user-input may be allowed in the input.
+  validate_required is a validation that tells Ecto which fields must be present from that list of fields'.
+  """
   def changeset(video, attrs) do
     video
     |> cast(attrs, [:url, :title, :description])
