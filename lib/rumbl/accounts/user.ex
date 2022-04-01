7b1acf8555742ed1e4a92256dcade8fa121a1da5
dennistarsang <sangdennis9@gmail.com>
2022-04-01 16:00:20 +0300

    ADDED: Added  to the changeset

--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -16,8 +16,17 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+    |> unique_constraint(:username)
   end
 
+  @spec registration_changeset(
+          {map, map}
+          | %{
+              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
+              optional(atom) => any
+            },
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
   def registration_changeset(user, params) do
     user
     |> changeset(params)
